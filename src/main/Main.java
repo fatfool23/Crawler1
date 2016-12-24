8bcbcd47d41725922249532fcd79a61295e126d7
Sami <samiwood96@gmail.com>
2016-12-24 16:35:20 -0600

    init commit

---
+++ src/main/Main.java
@@ -0,0 +1,27 @@
+package main;
+
+import datamanipulation.UrlFinder;
+import internet.PageReader;
+
+import java.net.URL;
+
+/**
+ *  driver class
+ *
+ * Created by Sami on 12/22/16.
+ */
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        String s = "https://en.wikipedia.org/wiki/Christmas_market";
+        PageReader pageReader = new PageReader(s);
+        //new UrlFinder().dumpPage(pageReader.getReader());
+        new UrlFinder().getUrls(pageReader.getReader());
+
+
+    }
+
+
+}
