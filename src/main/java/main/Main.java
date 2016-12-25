4c926d8e2860a0f79fe33bd72f0e08dfa45be013
GrayTurtle <john.tran@ou.edu>
2016-12-24 19:17:51 -0600

    formatting

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -11,7 +11,8 @@ import internet.PageReader;
 public class Main
 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         String s = "https://www.amazon.com"; //"https://en.wikipedia.org/wiki/Christmas_market";
         PageReader pageReader = new PageReader(s);
         //new UrlFinder().dumpPage(pageReader.getReader());
