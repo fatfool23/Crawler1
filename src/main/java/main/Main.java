e4fe3950246de3645a5c75d70fd5831143a3b2bf
Sami <samiwood96@gmail.com>
2016-12-24 18:12:08 -0600

    gradleized

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,8 +3,6 @@ package main;
 import datamanipulation.UrlFinder;
 import internet.PageReader;
 
-import java.net.URL;
-
 /**
  *  driver class
  *
@@ -15,7 +13,7 @@ public class Main
 
     public static void main(String[] args)
     {
-        String s = "https://en.wikipedia.org/wiki/Christmas_market";
+        String s = "https://www.amazon.com"; //"https://en.wikipedia.org/wiki/Christmas_market";
         PageReader pageReader = new PageReader(s);
         //new UrlFinder().dumpPage(pageReader.getReader());
         new UrlFinder().getUrls(pageReader.getReader());
